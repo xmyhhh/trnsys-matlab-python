081c15fa014af6fe19bb37ec9c7e0f5410fec063
xmyhhh <xmycissy@outlook.com>
2023-03-04 00:36:56 +0800

    update

--- s4/net.py
+++ s4/net.py
@@ -9,15 +9,17 @@ class MLP(nn.Module):
     def __init__(self, in_dim = 1, out_dim = 2):
         super(MLP, self).__init__()
         self.layer = nn.Sequential(
-            nn.Linear(in_dim, in_dim * 4),
+
+            nn.Linear(in_dim, in_dim * 32),
+
+            nn.BatchNorm1d(in_dim * 32),
             nn.ReLU(),
-            nn.Linear(in_dim * 4, in_dim * 8),
-            nn.ReLU(),
-            nn.Linear(in_dim * 8, out_dim * 4),
-            nn.ReLU(),
-            nn.Linear(out_dim * 4, out_dim),
+
+            nn.Linear(in_dim * 32, out_dim),
+            nn.Tanh()
         )
 
     def forward(self, input):
+
         return self.layer(input)
 
--- s4/train.py
+++ s4/train.py
@@ -11,19 +11,19 @@ from net import MLP
 import numpy as np
 from torch.optim import lr_scheduler
 
+
 def train(model, dataloader, loss_fn, optimizer, device):
     model.train()
     total_loss = 0
     index = 0
     for _, train_data in enumerate(dataloader):
-        index +=1
+        index += 1
         x = train_data["x"].to(device)
         y = train_data["y"].to(device)
         y_current = train_data["y_current"].to(device)
 
-        batch_size = x.shape[0]
         out = model(x)
-        Y_pred = out + y_current
+        Y_pred = out * torch.tensor([1.5, 0.1]).to(device) + y_current
 
         loss = loss_fn(Y_pred, y)
         total_loss += loss
@@ -31,7 +31,8 @@ def train(model, dataloader, loss_fn, optimizer, device):
         loss.backward()
         optimizer.step()
 
-    print('Aver Loss: %.3f ' % (total_loss / index)**0.5)
+    print('Aver Loss: %.3f ' % (total_loss / index) )
+
 
 if __name__ == '__main__':
     config_path = "config/default.yml"
@@ -44,18 +45,21 @@ if __name__ == '__main__':
 
     # 如果显卡可用，则用显卡进行训练
     device = "cuda" if torch.cuda.is_available() else 'cpu'
+
     # 调用 net 里定义的模型，如果 GPU 可用则将模型转到 GPU
     model = MLP(in_dim=12, out_dim=2).to(device)
     # 定义损失函数
-    loss_fn = nn.MSELoss()
+    loss_fn = nn.MSELoss(reduction='mean')
     # 定义优化器（SGD：随机梯度下降）
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     # 学习率每隔 10 个 epoch 变为原来的 0.1
-    lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.9)
     for t in tqdm(range(opt["train"]["predictor"]["max_epoch"])):
         lr_scheduler.step()
         print(f"Epoch {t + 1}\n----------------------")
         train(model, dataloader, loss_fn, optimizer, device)
         # test(test_dataloader, model, loss_fn)
+
+    torch.save(model, 'save.pt')
 
     print("Done!")
